c7f3fc7ab9f0a8fa34e2e0d4a56fcdb81dfc6a7c
geetu59 <geetanjaligomber98@gmail.com>
2024-03-23 23:44:36 +0530

    Refactor GildedRoseCBackstagePassesTest Unit Test 1. Rename test 2. Follow AAA: Arrange Act Assert 3. Create constant for item variables so that we should know what 15, 3 are 4. Extract Arrange in a method 5. Extract Assert in a method

--- src/test/java/com/a/introduction/gildedrose/GildedRoseCBackstagePassesTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseCBackstagePassesTest.java
@@ -1,45 +1,58 @@
 package com.a.introduction.gildedrose;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GildedRoseCBackstagePassesTest {
-	
-	@Test
-	public void testUpdateQualityBackstagePasses1() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("Backstage passes to a TAFKAL80ETC concert",
-				app.items[0].name);
-		assertEquals(14, app.items[0].sellIn);
-		assertEquals(4, app.items[0].quality);
-	}
 
-	@Test
-	public void testUpdateQualityBackstagePasses2() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 7, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("Backstage passes to a TAFKAL80ETC concert",
-				app.items[0].name);
-		assertEquals(6, app.items[0].sellIn);
-		assertEquals(5, app.items[0].quality);
-	}
+    public static final String ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    public static final int SELL_IN_1 = 15;
+    public static final int QUALITY = 3;
+    public static final int SELL_IN_2 = 7;
+    public static final int SELL_IN_3 = 4;
 
-	@Test
-	public void testUpdateQualityBackstagePasses3() {
-		Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 4, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals("Backstage passes to a TAFKAL80ETC concert",
-				app.items[0].name);
-		assertEquals(3, app.items[0].sellIn);
-		assertEquals(6, app.items[0].quality);
-	}
+    @Test
+    public void shouldDecreaseSellinBy1AndIncreaseQualityBy1WhenUnexpiredSellinIsProvided() {
+        GildedRose app = getGildedRose(ITEM_NAME, SELL_IN_1, QUALITY);
+
+        app.updateQuality();
+
+        Item expectedItem = new Item(ITEM_NAME, SELL_IN_1 - 1, QUALITY + 1);
+        assertItem(expectedItem, app.items[0]);
+    }
+
+    @Test
+    public void shouldDecreaseSellinBy1AndIncreaseQualityBy2WhenSellinIsProvided() {
+        GildedRose app = getGildedRose(ITEM_NAME, SELL_IN_2, QUALITY);
+
+        app.updateQuality();
+
+        Item expectedItem = new Item(ITEM_NAME, SELL_IN_2 - 1, QUALITY + 2);
+        assertItem(expectedItem, app.items[0]);
+    }
+
+    @Test
+    public void shouldDecreaseSellinBy1AndIncreaseQualityBy3WhenSellinIsProvided() {
+        GildedRose app = getGildedRose(ITEM_NAME, SELL_IN_3, QUALITY);
+
+        app.updateQuality();
+
+        Item expectedItem = new Item(ITEM_NAME, SELL_IN_3 - 1, QUALITY + 3);
+        assertItem(expectedItem, app.items[0]);
+    }
+
+    private static void assertItem(Item expectedItem, Item actualItem) {
+        assertEquals(expectedItem.name, actualItem.name);
+        assertEquals(expectedItem.sellIn, actualItem.sellIn);
+        assertEquals(expectedItem.quality, actualItem.quality);
+    }
+
+    private static GildedRose getGildedRose(String itemName, int sellIn1, int quality) {
+        Item item = new Item(itemName, sellIn1, quality);
+        Item[] items = new Item[]{item};
+        GildedRose app = new GildedRose(items);
+        return app;
+    }
 
 }
