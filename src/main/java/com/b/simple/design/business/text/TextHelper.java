f20f65630ba731ceef3d35477739584eb420d093
geetu59 <geetanjaligomber98@gmail.com>
2024-03-24 22:16:52 +0530

    Write swap last 2 chars method

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -2,11 +2,15 @@ package com.b.simple.design.business.text;
 
 public class TextHelper {
 
-	public String swapLastTwoCharacters(String str) {
-		return null;
-	}
+    public String swapLastTwoCharacters(String str) {
+        if (str.length() < 2) return str;
+        char lastChar = str.charAt(str.length() - 1);
+        char secondLastChar = str.charAt(str.length() - 2);
+        String restOfString = str.substring(0, str.length() - 2);
+        return restOfString + lastChar + secondLastChar;
+    }
 
-	public String truncateAInFirst2Positions(String str) {
-		return null;
-	}
+    public String truncateAInFirst2Positions(String str) {
+        return null;
+    }
 }
