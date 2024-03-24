d32b633f85aaa73bac1c844fb34ef627e740f758
geetu59 <geetanjaligomber98@gmail.com>
2024-03-24 18:31:52 +0530

    Refactor StudentHelper Problem 2

--- src/main/java/com/b/simple/design/business/student/StudentHelper.java
+++ src/main/java/com/b/simple/design/business/student/StudentHelper.java
@@ -9,6 +9,10 @@ public class StudentHelper {
     /* PROBLEM 1 */
     /*
      * You get a grade B if marks are between 51 and 80 (both inclusive). Except for Maths where the upper limit is increased by 10.
+     * Refactoring:
+     * 1. Make constants for lower and upper limit.
+     * 2. Check for isMaths, changes upper limit, so extract it out and update upper limit accordingly to isMaths check
+     * 3. In upper limit also, some extra is getting added, based on isMaths condition and hence further extract it out.
      */
     public boolean isGradeB(int marks, boolean isMaths) {
         int extraLimit = isMaths ? EXTRA_LIMIT : 0;
@@ -21,29 +25,26 @@ public class StudentHelper {
 	You are awarded a grade based on your marks.
 	Grade A = 91 to 100, Grade B = 51 to 90, Otherwise Grade C
 	Except for Maths where marks to get a Grade are 5 higher than required for other subjects.
+	Refactor
+	1. Fns isGradeA are very small, so inline them.
+	2. Naming of methods should be consistent.
+	3. Not removing comment as it is telling business logic
+	4. Since we have to return grade only, so no need to put it in variable, directly return
+	5. Since, we directly return, so no need of else if
+	6. If you see, 90+5, 50+5, so extra limit is 5 and hence extract it
+	7. isMaths check if for extra limit and hence put it in extraLimit
+	8. Inline lowerLimitForAGrade
+	9. Dont need marks>90 check as first if will return if that's the case
 	*/
 
     public String getGrade(int mark, boolean isMaths) {
-        String grade = "C";
-
-        if (isGradeA(mark, isMaths))
-            grade = "A";
-        else if (isBGrade(mark, isMaths)) {
-            grade = "B";
+        int extraLimit = isMaths ? 5 : 0;
+        if (mark > 90 + extraLimit)
+            return "A";
+        if (mark > 50 + extraLimit) {
+            return "B";
         }
-        return grade;
-    }
-
-    private boolean isGradeA(int mark, boolean isMaths) {
-        int lowerLimitForAGrade = isMaths ? 95
-                : 90;
-        return mark > lowerLimitForAGrade;
-    }
-
-    private boolean isBGrade(int mark, boolean isMaths) {
-        int lowerLimitGradeB = isMaths ? 55
-                : 50;
-        return mark > lowerLimitGradeB && mark < 90;
+        return "C";
     }
 
     /*  PROBLEM 3
