83e3c0dbad689b3f9ecebb4fc48de8fd63787e69
geetu59 <geetanjaligomber98@gmail.com>
2024-03-25 22:31:34 +0530

    Refactor CustomerBOImpl

--- src/main/java/com/b/simple/design/business/customer/CustomerBOImpl.java
+++ src/main/java/com/b/simple/design/business/customer/CustomerBOImpl.java
@@ -1,43 +1,52 @@
 package com.b.simple.design.business.customer;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import com.b.simple.design.business.exception.DifferentCurrenciesException;
 import com.b.simple.design.model.customer.Amount;
 import com.b.simple.design.model.customer.AmountImpl;
 import com.b.simple.design.model.customer.Currency;
 import com.b.simple.design.model.customer.Product;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class CustomerBOImpl implements CustomerBO {
+    /*Refactor:
+     * 1. This method has 3 parts, a. no products are there b. diff currency is there c. return totalAmount
+     * a part is fine.
+     * 2. Rename temp to totalAmount
+     * 3. Remove comments
+     * 4. For b and c, use java streams*/
+    @Override
+    public Amount getCustomerProductsSum(List<Product> products) throws DifferentCurrenciesException {
+        BigDecimal totalAmount = BigDecimal.ZERO;
 
-	@Override
-	public Amount getCustomerProductsSum(List<Product> products)
-			throws DifferentCurrenciesException {
-		BigDecimal temp = BigDecimal.ZERO;
+        if (products.isEmpty()) return new AmountImpl(totalAmount, Currency.EURO);
 
-		if (products.size() == 0)
-			return new AmountImpl(temp, Currency.EURO);
+        if (!doAllProductsHaveSameCurrency(products)) {
+            throw new DifferentCurrenciesException();
+        }
 
-		// Throw Exception If Any of the product has a currency different from
-		// the first product
-		Currency firstProductCurrency = products.get(0).getAmount()
-				.getCurrency();
 
-		for (Product product : products) {
-			boolean currencySameAsFirstProduct = product.getAmount()
-					.getCurrency().equals(firstProductCurrency);
-			if (!currencySameAsFirstProduct) {
-				throw new DifferentCurrenciesException();
-			}
-		}
+        return getAmount(products);
+    }
 
-		// Calculate Sum of Products
-		for (Product product : products) {
-			temp = temp.add(product.getAmount().getValue());
-		}
-		
-		// Create new product
-		return new AmountImpl(temp, firstProductCurrency);
-	}
+    private static AmountImpl getAmount(List<Product> products) {
+        BigDecimal totalAmount;
+        Currency firstProductCurrency = products.get(0).getAmount()
+                .getCurrency();
+
+        totalAmount = products.stream().map(product ->
+                        product.getAmount().getValue())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return new AmountImpl(totalAmount, firstProductCurrency);
+    }
+
+    private static boolean doAllProductsHaveSameCurrency(List<Product> products) {
+        Currency firstProductCurrency = products.get(0).getAmount()
+                .getCurrency();
+
+        return products.stream().map(product -> product.getAmount().getCurrency())
+                .allMatch(currency -> currency.equals(firstProductCurrency));
+    }
 }
