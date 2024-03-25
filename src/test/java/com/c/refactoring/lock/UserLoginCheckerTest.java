f18ae19a6a3b96c6102670a9d83b9e2a18b8f6bd
geetu59 <geetanjaligomber98@gmail.com>
2024-03-25 16:30:25 +0530

    Refactor UserLoginCheckerTest

--- src/test/java/com/c/refactoring/lock/UserLoginCheckerTest.java
+++ src/test/java/com/c/refactoring/lock/UserLoginCheckerTest.java
@@ -1,66 +1,80 @@
 package com.c.refactoring.lock;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Date;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserLoginCheckerTest {
+    public static final int ID = 10;
+    public static final String STATUS = "NOT_USED";
     UserLoginChecker userLoginChecker = new UserLoginChecker();
 
     @Test
     public void testisUserAllowedToLogin_DifferentUserTriesImmediatelyAfter() {
-        Object[] access = new Object[] { "TEST_USER_ID_1", new Date() };
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", true, new User(
-                "TEST_USER_ID_2"), Arrays.asList(new Object[][] { access }));
-        assertTrue(lock.isReadAccess());
-        assertNotNull(lock.getLockReason());
+        Object[] existingLock = new Object[]{"TEST_USER_ID_1", new Date()};
+
+        Lock lock = userLoginChecker.isUserAllowedToLogin(ID, STATUS, true, new User(
+                "TEST_USER_ID_2"), Arrays.asList(new Object[][]{existingLock}));
+
+        assertReadAccess(lock);
     }
 
     @Test
     public void testisUserAllowedToLogin_SameUserReturnsToFirstScreen() {
-        Object[] access = new Object[] { "TEST_USER_ID", new Date() };
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", true, new User(
-                "TEST_USER_ID"), Arrays.asList(new Object[][] { access }));
-        assertFalse(lock.isReadAccess());
-        assertNull(lock.getLockReason());
+        Object[] existingLock = new Object[]{"TEST_USER_ID", new Date()};
+
+        Lock lock = userLoginChecker.isUserAllowedToLogin(ID, STATUS, true, new User(
+                "TEST_USER_ID"), Arrays.asList(new Object[][]{existingLock}));
+
+        assetWriteAccess(lock);
     }
 
     @Test
     public void testisUserAllowedToLogin_SameUserReturnsToSecondScreen() {
-        Object[] access = new Object[] { "TEST_USER_ID", new Date() };
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", false, new User(
-                "TEST_USER_ID"), Arrays.asList(new Object[][] { access }));
-        assertFalse(lock.isReadAccess());
-        assertNull(lock.getLockReason());
+        Object[] existingLock = new Object[]{"TEST_USER_ID", new Date()};
+
+        Lock lock = userLoginChecker.isUserAllowedToLogin(ID, STATUS, false, new User(
+                "TEST_USER_ID"), Arrays.asList(new Object[][]{existingLock}));
+
+        assetWriteAccess(lock);
     }
 
     @Test
     public void testisUserAllowedToLogin_User2TriesToLoginToFirstScreen3hoursAfterUser1() {
-        Object[] access = new Object[] { "TEST_USER_ID_1", threeHoursBefore() };
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", true, new User(
-                "TEST_USER_ID_2"), Arrays.asList(new Object[][] { access }));
-        assertFalse(lock.isReadAccess());
-        assertNull(lock.getLockReason());
+        Object[] existingLock = new Object[]{"TEST_USER_ID_1", threeHoursBefore()};
+
+        Lock lock = userLoginChecker.isUserAllowedToLogin(ID, STATUS, true, new User(
+                "TEST_USER_ID_2"), Arrays.asList(new Object[][]{existingLock}));
+
+        assetWriteAccess(lock);
     }
 
     @Test
     public void testisUserAllowedToLogin_User2TriesToLoginToSecondScreen3hoursAfterUser1() {
-        Object[] access = new Object[] { "TEST_USER_ID_1", threeHoursBefore() };
-        Lock lock = userLoginChecker.isUserAllowedToLogin(10, "NOT_USED", false, new User(
-                "TEST_USER_ID_2"), Arrays.asList(new Object[][] { access }));
-        assertTrue(lock.isReadAccess());
-        assertNotNull(lock.getLockReason());
+        Object[] existingLock = new Object[]{"TEST_USER_ID_1", threeHoursBefore()};
+
+        Lock lock = userLoginChecker.isUserAllowedToLogin(ID, STATUS, false, new User(
+                "TEST_USER_ID_2"), Arrays.asList(new Object[][]{existingLock}));
+
+        assertReadAccess(lock);
     }
 
     public Date threeHoursBefore() {
         Date now = new Date();
         return new Date(now.getTime() - 3 * 60 * 60 * 1000);
+    }
+
+    private static void assetWriteAccess(Lock lock) {
+        assertFalse(lock.isReadAccess());
+        assertNull(lock.getLockReason());
+    }
+
+    private static void assertReadAccess(Lock lock) {
+        assertTrue(lock.isReadAccess());
+        assertNotNull(lock.getLockReason());
     }
 
 }
