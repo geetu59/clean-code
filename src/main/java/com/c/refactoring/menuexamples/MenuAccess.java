7a12ea7f461d922ba20010aa9cab984b858540fc
geetu59 <geetanjaligomber98@gmail.com>
2024-03-26 10:14:29 +0530

    Refactor MenuAccess

--- src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
@@ -1,34 +1,33 @@
 package com.c.refactoring.menuexamples;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MenuAccess {
 
-    public void setAuthorizationsInEachMenus(
-            List<MenuItem> menuItemsList, Role[] roles) {
-        for (int i = 0; i < menuItemsList.size(); i++) {
-            MenuItem menuItem = menuItemsList.get(i);
-            if (roles != null) {
-                for (int j = 0; j < roles.length; j++) {
-                    if (roles[j].getName().equals(menuItem
-                            .getReadAccessRole())
-                            && !Constants.WRITE.equals(
-                                    menuItem
-                                            .getAccess())) {
-                        menuItem.setAccess(Constants.READ);
-                        menuItem.setVisible(true);
-                    } else if (roles[j].getName().equals(
-                            menuItem
-                                    .getWriteAccessRole())) {
-                        menuItem.setAccess(Constants.WRITE);
-                        menuItem.setVisible(true);
-                    }
-                }
+    /*Refactor:
+    * 1. When role is not null then we do things, so move that condition upwards
+    * 2. Use java streams fore
+    * 3. Remove else if as both are diff statements
+    * 4. See if roles has corresponding role for which we look for, move that if condition into method*/
+    public void setAuthorizationsInEachMenus(List<MenuItem> menuItemsList, Role[] roles) {
+        if (roles == null) return;
 
+        menuItemsList.forEach(menuItem -> {
+            if (hasRequiredAccessRole(roles, menuItem.getReadAccessRole())) {
+                menuItem.setAccess(Constants.READ);
+                menuItem.setVisible(true);
             }
+            if (hasRequiredAccessRole(roles, menuItem.getWriteAccessRole())) {
+                menuItem.setAccess(Constants.WRITE);
+                menuItem.setVisible(true);
+            }
+        });
 
-        }
+    }
 
+    private static boolean hasRequiredAccessRole(Role[] roles, String roleToCheckFor) {
+        return Arrays.stream(roles).anyMatch(role -> role.getName().equals(roleToCheckFor));
     }
 
 }
