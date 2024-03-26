c6b00a3a312c08161e3525b64adae983e2d2ceb7
geetu59 <geetanjaligomber98@gmail.com>
2024-03-26 08:42:51 +0530

    Refactor Movie

--- src/main/java/com/c/refactoring/movie/Movie.java
+++ src/main/java/com/c/refactoring/movie/Movie.java
@@ -2,8 +2,12 @@ package com.c.refactoring.movie;
 
 import com.c.refactoring.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Movie {
 
+    public static final List<String> VALID_B_RATINGS = Arrays.asList("B1", "B2", "B3", "B4");
     String rating;
 
     public Movie(String rating) {
@@ -17,23 +21,35 @@ public class Movie {
 
     /*Axx or By
     Where x represents any digit between 0 and 9, and y represents 
-    any digit between 1 and 4*/
+    any digit between 1 and 4
+    Refactor:
+    1. Remove first condition of rating!=null, beside add rating ==null on top only
+    2. Since this.getRating is used frequently, put it in variable
+    3. Since, if else  if both are independent logical statements, make other if only
+    4. Move logic of if in other method
+    5. For B rating, we know it can have only 4 possible values, so put them in list and check if your rating is there
+    beside of doing substring and putting multiple conditions*/
     public boolean isValidRating() {
-        if (this.getRating() != null) {
-            if (this.getRating().substring(0, 1).equalsIgnoreCase("B")
-                    && this.getRating().length() == 2) {
-                if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-                        && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-                        && Integer.parseInt(this.getRating().substring(1, 2)) < 5)
-                    return true;
+        String rating = this.getRating();
 
-            } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-                    && this.getRating().length() == 3
-                    && StringUtils.isNumeric(this.getRating().substring(1, 3)))
-                return true;
+        if (rating == null) return false;
 
-        }
+        if (isBRating(rating)) return true;
+
+        if (isARating(rating)) return true;
+
         return false;
+    }
+
+    private static boolean isBRating(String rating) {
+        return VALID_B_RATINGS.contains(rating);
+    }
+
+    private static boolean isARating(String rating) {
+        String firstChar = rating.substring(0, 1);
+        return firstChar.equalsIgnoreCase("A")
+                && rating.length() == 3
+                && StringUtils.isNumeric(rating.substring(1, 3));
     }
 
     public void setRating(String rating) {
